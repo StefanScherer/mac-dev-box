1331f002ec2fb6878b91b9e29b199e182ef8738e
Stefan Scherer <scherer_stefan@icloud.com>
2015-08-11 19:45:47 +0200

    Use upstream docker-compose 1.4.0

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,4 +2,6 @@
 
 brew install docker
 brew install docker-machine
-brew install docker-compose
+# brew install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
